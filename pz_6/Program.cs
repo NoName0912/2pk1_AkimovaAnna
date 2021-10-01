bab05245f5af915a9a51833c25dce92bd2809499
Gospodin Gospodin <iseeyoutear.04@mail.ru>
2021-10-01 08:56:36 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+
+            int i = 1;
+            while (i * i <= n)
+            {
+                Console.WriteLine((i * i++).ToString());
+            }
+        }
+    }
+}
