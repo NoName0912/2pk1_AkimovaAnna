77498108b938f67d81c4430aef8c67f0f8aa4a74
Gospodin Gospodin <iseeyoutear.04@mail.ru>
2021-10-15 12:25:12 +0500

    test edit

---
+++ pz_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{i + 1}:\t");
+
+                int n = int.Parse(Console.ReadLine());
+
+                if (n == 0)
+                {
+                    break;
+                }
+
+                numbers[i] = n;
+            }
+
+            foreach (int n in numbers)
+            {
+                total += n;
+            }
+
+            Console.WriteLine($"Сумма {total}");
+            Console.ReadKey();
+        }
+    }
+}
