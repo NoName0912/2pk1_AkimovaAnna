cbfab1ee06607b3f25d1edd9c4a64d3d3b7cd8b5
Gospodin Gospodin <iseeyoutear.04@mail.ru>
2021-11-19 21:40:15 +0500

    test edit

---
+++ pz_15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = File.ReadAllLines(@"C:\Users\Samsung\Desktop\new_file.txt").Length;
+            Console.WriteLine(count);
+
+
+            var words = File.ReadAllText(@"C:\Users\Samsung\Desktop\new_file.txt")
+            .Split(" ,.!?';:\"\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => !x.Any(Char.IsDigit));
+
+            Console.WriteLine(words.Count());
+            Console.ReadLine();
+
+        }
+    }
+}
