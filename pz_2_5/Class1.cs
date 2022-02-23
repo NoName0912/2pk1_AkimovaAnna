1b9e6004c9126649bf01ca2b7d4e964009144f41
Gospodin Gospodin <iseeyoutear.04@mail.ru>
2022-02-23 18:05:36 +0500

    test edit

---
+++ pz_2_5/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_2_5
+{
+    class Class1
+    {
+
+        internal class FactionalNumber
+        {
+            public char sign;
+            public int dividend;
+            public int divider;
+
+            static int CounterPlus;
+            static int CounterMinus;
+
+            public double Dividend
+            {
+                get { return dividend; }
+                set
+                {
+                    if (value.GetTypeCode() == TypeCode.Double & (value % 2 == 0 || value % 3 == 0 || value % 5 == 0 || value % 7 == 0))
+                    {
+                        dividend = Convert.ToInt32(value);
+                    }
+                    else
+                        Console.WriteLine("Error.");
+                }
+            }
+
+            public double Divider
+            {
+                get { return divider; }
+                set
+                {
+                    if (value.GetTypeCode() == TypeCode.Double & (value % 2 == 0 || value % 3 == 0 || value % 5 == 0 || value % 7 == 0) & value != 0)
+                    {
+                        divider = Convert.ToInt32(value);
+                    }
+                    else
+                        Console.WriteLine("Error.");
+                }
+            }
+            public virtual char Sign
+            {
+                set
+                {
+                    if (value == '-')
+                    {
+                        CounterMinus++;
+                        sign = value;
+                    }
+                    else
+                        CounterPlus++;
+                }
+            }
+
+            public FactionalNumber(char a)
+            {
+                Sign = a;
+                Dividend = 50;
+                Divider = 7;
+            }
+
+            public FactionalNumber()
+            {
+                Console.WriteLine(CounterPlus);
+                Console.WriteLine(CounterMinus);
+            }
+
+            public FactionalNumber(char a, double x, double n)
+            {
+                Sign = a;
+                Dividend = x;
+                Divider = n;
+            }
+
+            public void FactionalSymbol()
+            {
+                Console.WriteLine(CounterPlus);
+                Console.WriteLine(CounterMinus);
+            }
+
+
+            public virtual void GetNumber()
+            {
+                Console.WriteLine($"{sign}{dividend}/{divider}");
+            }
+        }
+    }
+}
---
+++ pz_2_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using class1;
+using class2;
+
+namespace pz_2_5
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите знак + или - ");
+                char sign1 = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Введите первое значение: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите второе значение: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                FactionalNumber factional1 = new FactionalNumber(sign1, a, b);
+                factional1.GetNumber();
+                Console.WriteLine("");
+            }
+            FactionalNumber factional2 = new FactionalNumber();
+
+            var negFN = new NegativeFN(8, 10);
+            negFN.GetNumber();
+        }
+    }
+}
